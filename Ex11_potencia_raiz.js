6bb6b72305d13d0e08432ab05ebb475a3a8769dd
Rafael Santos <rafael.professor1960@gmail.com>
2025-02-13 16:29:46 -0300

    Ex11_potencia_raiz.js

---
+++ Ex11_potencia_raiz.js
@@ -0,0 +1,10 @@
+function calcularPotencia(base, expoente) {
+    return Math.pow(base, expoente);
+}
+
+function calcularRaiz(numero) {
+    return Math.sqrt(numero);
+}
+
+console.log(`2^3 = ${calcularPotencia(2, 3)}`); // 2³ = 8
+console.log(`Raiz de 25 = ${calcularRaiz(25)}`); // √25 = 5
